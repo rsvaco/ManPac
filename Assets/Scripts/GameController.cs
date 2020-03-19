159d0ed557362e465f608d438f65a0519a5c3621
rsvaco <48381233+rsvaco@users.noreply.github.com>
2020-03-19 20:08:42 +0100

    cambios al gamecontroller
    cambios varios al gamecontroller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,12 +9,23 @@ public class GameController : MonoBehaviour
     [RangeAttribute(0, 4)]
     public int numJugadores;
 
+    public GameObject[] spawns;
+    public GameObject[] spawnsFantasmas;
+
+    public GameObject pacoPrefab;
+    public GameObject fantasmaPrefab;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < numJugadores; i++)
+        {
+            GameObject paco = Instantiate(pacoPrefab, spawns[i].transform.position, Quaternion.identity);
+            //paco.GetComponent<SpriteRenderer>().color = colors[i];
+            GameObject fant = Instantiate(fantasmaPrefab, spawnsFantasmas[i].transform.position, Quaternion.identity);
+            //fant.GetComponent<SpriteRenderer>().color = colors[i];
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Paco.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paco : MonoBehaviour
+{
+    public int puntos;
+    public int banco;
+    public int equipo;
+}
