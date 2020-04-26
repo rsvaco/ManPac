77b4fcfbde11d4e4e26a324a17b0fb9628f8ed58
rsvaco <48381233+rsvaco@users.noreply.github.com>
2020-04-26 22:13:24 +0200

    configurarpartida casi terminado

---
+++ Assets/Scripts/ConfigurarPartida.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfigurarPartida : MonoBehaviour
+{
+    public int segundosMax = 6000;
+
+    //mantener mismo orden
+    public GameObject[] listaImagenes;
+    public int[] listaImagenesOrdenDeCarga;
+
+
+    private int numJugadores = 1;
+    private int segundosTotal = 120;
+    private int mapaSeleccionado = 0;
+
+    public TextMeshProUGUI text_jugadores, text_segundos;
+    public GameObject img_mapa;
+
+    public void addSegundos(int cantidad) {
+        segundosTotal = Mathf.Min(segundosMax, segundosTotal + cantidad);
+        updateSegundos();
+    }
+
+    public void quitarSegundos(int cantidad)
+    {
+        segundosTotal = Mathf.Max(30, segundosTotal - cantidad);
+        updateSegundos();
+    }
+
+    private void updateSegundos() {
+        int minutos = (int)Mathf.Floor(segundosTotal / 60);
+        int segundos = segundosTotal % 60;
+        string stringSegundos = "00" + segundos;
+        stringSegundos = stringSegundos.Substring(stringSegundos.Length - 2);
+        text_segundos.text = "" + minutos + ":" + stringSegundos;
+    }
+
+    public void siguienteMapa() {
+        if (mapaSeleccionado == listaImagenes.Length - 1) {
+            mapaSeleccionado = 0;
+        }
+        else
+        {
+            mapaSeleccionado++;
+        }
+        updateMapa();
+    }
+
+    public void anteriorMapa() {
+        if (mapaSeleccionado == 0)
+        {
+            mapaSeleccionado = listaImagenes.Length - 1;
+        }
+        else {
+            mapaSeleccionado--;
+        }
+        updateMapa();
+    }
+
+    private void updateMapa() {
+        for (int i = 0; i < listaImagenes.Length; i++) {
+            if (i == mapaSeleccionado)
+            {
+                listaImagenes[i].SetActive(true);
+            }
+            else {
+                listaImagenes[i].SetActive(false);
+            }
+        }
+    }
+
+    public void addJugadores(int cantidad)
+    {
+        numJugadores = Mathf.Min(4, numJugadores + cantidad);
+        text_jugadores.text = numJugadores.ToString();
+    }
+
+    public void quitarJugadores(int cantidad)
+    {
+        numJugadores = Mathf.Max(1, numJugadores - cantidad);
+        text_jugadores.text = numJugadores.ToString();
+    }
+
+
+    void Start()
+    {
+        text_jugadores.text = numJugadores.ToString();
+        updateSegundos();
+        updateMapa();
+    }
+
+
+}
--- Assets/Scripts/GlobalOptions.cs
+++ Assets/Scripts/GlobalOptions.cs
@@ -12,6 +12,7 @@ public class GlobalOptions : MonoBehaviour
     public Color[] playerColors = {new Color(1f,1f,0f), new Color(1f,0f,0f), new Color(0f,1f,1f), new Color(1f,0f,0.5f) };
     public Color mainColor = new Color(0.2f,0f,1f), backColor = new Color(0f, 0f, 0f);
     public int numJugadores = 4;
+    public int segundosMax = 2 * 60;
 
     /**********************/
     void Start() {
